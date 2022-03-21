a1caed3909c188fef3e5bcd34e910316c57fd9c7
prasenjit52282 <prasenjitkarmakar52282@gmail.com>
2022-03-21 16:02:52 +0530

    code has all experiments from the conf paper

--- library/experiments.py
+++ library/experiments.py
@@ -12,13 +12,36 @@ def run_experiment_with(model,city,train_devices,test_devices,epochs=None,batch_
     perf["test_dev"]=test_devices
     return perf
 
-def overall_experiments(exp_name,model,city,epochs=None,batch_size=None):
+def read_overall_confs(city):
+    exps={}
     devices=sorted([int(f.split("_")[1]) for f in glob.glob(f"./Data/{city}/*")])
-    overall_exp={}
     for dev in devices:
-        overall_exp[dev]=dict(train_devices=list(set(devices)-set([dev])),test_devices=[dev])
+        exps[dev]=dict(train_devices=list(set(devices)-set([dev])),test_devices=[dev])
+    return exps
+
+def read_similarity_confs(city,similarity):
+    exps={}
+    temp=dict(pd.read_csv(f"./Data/metadata/{city}_{similarity}.csv").T)
+    for e,conf in temp.items():
+        exps[e]=dict(train_devices=[conf["train_devices"]],test_devices=[conf["test_devices"]])
+    return exps
+
+def get_experiment_config(exp_name,city):
+    if 'overall' in exp_name:
+        return read_overall_confs(city)
+    elif 'sim' in exp_name:
+        return read_similarity_confs(city,'sim')
+    elif 'dis' in exp_name:
+        return read_similarity_confs(city,'dis')
+
+def experiment(exp_name,city,model,epochs=None,batch_size=None):
+    """
+    exp_name="(overall/sim/dis)_*"
+    city=("Delhi"/"Dgp")
+    """
     stat=[]
-    for exp, conf in overall_exp.items():
+    exp_config=get_experiment_config(exp_name,city)
+    for exp, conf in exp_config.items():
         met=run_experiment_with(model,city,conf["train_devices"],conf["test_devices"],epochs,batch_size)
         stat.append(met)
     df=pd.DataFrame(stat)
